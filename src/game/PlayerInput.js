02afdf5b49aa5452baf83fe5ea0f0b0374f6f237
apo91 <andrii91@protonmail.com>
2021-06-04 14:33:04 +0300

    Initial PlayerInput

---
+++ src/game/PlayerInput.js
@@ -0,0 +1,50 @@
+import EventEmitter from "events";
+import { DIRECTION } from "./direction";
+
+export class PlayerInput extends EventEmitter {
+    constructor() {
+        this.setupEventListeners();
+    }
+    setupEventListeners() {
+        window.addEventListener('keydown', this.handleWindowKeyDown);
+    }
+    clearEventListeners() {
+        window.removeEventListener('keydown', this.handleWindowKeyDown);
+    }
+    /**
+     * 
+     * @param {KeyboardEvent} e 
+     */
+    handleWindowKeyDown = (e) => {
+        switch (e.code) {
+            case "Numpad8":
+            case "ArrowUp":
+                this.emit("move", DIRECTION.NORTH);
+                break;
+            case "Numpad6":
+            case "ArrowRight":
+                this.emit("move", DIRECTION.EAST);
+                break;
+            case "Numpad2":
+            case "ArrowDown":
+                this.emit("move", DIRECTION.SOUTH);
+                break;
+            case "Numpad4":
+            case "ArrowLeft":
+                this.emit("move", DIRECTION.WEST);
+                break;
+            case "Numpad9":
+                this.emit("move", DIRECTION.NORTH_EAST);
+                break;
+            case "Numpad3":
+                this.emit("move", DIRECTION.SOUTH_EAST);
+                break;
+            case "Numpad1":
+                this.emit("move", DIRECTION.SOUTH_WEST);
+                break;
+            case "Numpad7":
+                this.emit("move", DIRECTION.NORTH_WEST);
+                break;
+        }
+    }
+}
--- src/game/direction.js
+++ src/game/direction.js
@@ -3,4 +3,8 @@ export const DIRECTION = {
     EAST: 1,
     SOUTH: 2,
     WEST: 3,
+    NORTH_EAST: 4,
+    SOUTH_EAST: 5,
+    SOUTH_WEST: 6,
+    NORTH_WEST: 7,
 };
