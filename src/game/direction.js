7c3bd640be207bf752ba28db0de9a2dc619b0ab2
apo91 <andrii91@protonmail.com>
2021-06-06 13:39:49 +0300

    Add direction offsets mapping

--- src/game/direction.js
+++ src/game/direction.js
@@ -8,3 +8,14 @@ export const DIRECTION = {
     SOUTH_WEST: 6,
     NORTH_WEST: 7,
 };
+
+export const DIRECTION_OFFSETS = {
+    [DIRECTION.NORTH]: [0, -1],
+    [DIRECTION.EAST]: [1, 0],
+    [DIRECTION.SOUTH]: [0, 1],
+    [DIRECTION.WEST]: [-1, 0],
+    [DIRECTION.NORTH_EAST]: [1, -1],
+    [DIRECTION.SOUTH_EAST]: [1, 1],
+    [DIRECTION.SOUTH_WEST]: [-1, 1],
+    [DIRECTION.NORTH_WEST]: [-1, -1],
+};
