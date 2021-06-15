55adc575aaf0aef4ae350c73e783140003bc3956
apo91 <andrii91@protonmail.com>
2021-06-15 08:17:28 +0300

    Prerender tiles into cache

--- src/game/rendering/AsciiRenderer.js
+++ src/game/rendering/AsciiRenderer.js
@@ -57,11 +57,11 @@ const tileTypeSymbol = (tileType) => {
 
 /**
  * @param {TileType} tileType
- * @param {EntityLayers} entityLayers
+ * @param {EntityLayers} [entityLayers]
  * @returns {string}
  */
 const tileSymbol = (tileType, entityLayers) => {
-    if (entityLayers.isEmpty()) {
+    if (!entityLayers || entityLayers.isEmpty()) {
         return tileTypeSymbol(tileType);
     } else {
         const topEntity = entityLayers.getTopEntity();
@@ -106,13 +106,14 @@ export class AsciiRenderer {
         this.viewportWidth = viewportConfig.width;
         this.viewportHeight = viewportConfig.height;
         this.wallBackgroundGradient = [0, 0, 0];
-        this.bgCacheOffsetX = Math.floor(this.viewportWidth / 2);
-        this.bgCacheOffsetY = Math.floor(this.viewportHeight / 2);
-        this.bgCacheWidth = this.dungeon.width + viewportConfig.width;
-        this.bgCacheHeight = this.dungeon.height * viewportConfig.height;
-        this.bgCache = new Array(this.bgCacheWidth * this.bgCacheHeight);
+        this.tilesCacheOffsetX = Math.floor(this.viewportWidth / 2);
+        this.tilesCacheOffsetY = Math.floor(this.viewportHeight / 2);
+        this.tilesCacheWidth = this.dungeon.width + viewportConfig.width;
+        this.tilesCacheHeight = this.dungeon.height + viewportConfig.height;
+        this.tilesCache = new Array(this.tilesCacheWidth * this.tilesCacheHeight);
         this.noise = new Noise(rng.next());
         this.generateColours(rng);
+        this.prerender();
     }
     /**
      * @param {Random} rng
@@ -125,13 +126,15 @@ export class AsciiRenderer {
     /**
      * @param {number} x
      * @param {number} y
+     * @param {boolean} includeEntities
      */
-    getWallBackgroundColor = (x, y) => {
-        const cacheIndex = (this.bgCacheOffsetY + y) * this.bgCacheWidth + (this.bgCacheOffsetX + x);
-        const cachedValue = this.bgCache[cacheIndex];
-        if (cachedValue) {
-            return cachedValue;
-        } else {
+    renderTile(x, y, includeEntities) {
+        const isOutOfBounds = this.isOutOfBounds(x, y);
+        const tileIndex = this.dungeon.getIndexFromCoords(x, y);
+        const isWall = isOutOfBounds ||
+            this.dungeon.layoutTilesBuffer[tileIndex] === TileType.Wall;
+        let bgColor = "default";
+        if (isWall) {
             const nx = x / this.dungeon.width;
             const ny = y / this.dungeon.height;
             const z = fbm(3, 11.29, 0.51, nx, ny, (x, y) => (1 + this.noise.perlin2(x, y)) / 2);
@@ -139,9 +142,74 @@ export class AsciiRenderer {
             const r = z * grad[0] * 255;
             const g = z * grad[1] * 255;
             const b = z * grad[2] * 255;
-            const bgColor = `rgb(${r}, ${g}, ${b})`;
-            this.bgCache[cacheIndex] = bgColor;
-            return bgColor;
+            bgColor = `rgb(${r}, ${g}, ${b})`;
+        }
+        if (isOutOfBounds) {
+            return (
+                <RenderedTile
+                    key={y * this.viewportWidth + x}
+                    tilesPerRow={this.viewportWidth}
+                    color={tileForegroundColor(TileType.Wall)}
+                    backgroundColor={bgColor}
+                >
+                    #
+                </RenderedTile>
+            );
+        } else {
+            const layoutTile = this.dungeon.layoutTilesBuffer[tileIndex];
+            const entityLayers = this.dungeon.entityLayersBuffer[tileIndex];
+            return (
+                <RenderedTile
+                    key={y * this.viewportWidth + x}
+                    tilesPerRow={this.viewportWidth}
+                    color={
+                        includeEntities
+                            ? tileForegroundColor(layoutTile, entityLayers)
+                            : tileForegroundColor(layoutTile)
+                    }
+                    backgroundColor={bgColor}
+                >
+                    {
+                        includeEntities
+                            ? tileSymbol(layoutTile, entityLayers)
+                            : tileSymbol(layoutTile)
+                    }
+                </RenderedTile>
+            );
+        }
+    }
+    /**
+     * @param {number} x
+     * @param {number} y
+     * @returns {boolean}
+     */
+    isOutOfBounds(x, y) {
+        return x < 0 || x >= this.dungeon.width
+            || y < 0 || y >= this.dungeon.height;
+    }
+    /**
+     * @param {number} x
+     * @param {number} y
+     * @returns {boolean}
+     */
+    isStatic(x, y) {
+        if (this.isOutOfBounds(x, y)) {
+            return true;
+        } else {
+            const tileIndex = this.dungeon.getIndexFromCoords(x, y);
+            const entityLayers = this.dungeon.entityLayersBuffer[tileIndex];
+            return entityLayers.isEmpty();
+        }
+    }
+    prerender() {
+        for (let dy = 0; dy < this.tilesCacheHeight; dy++) {
+            for (let dx = 0; dx < this.tilesCacheWidth; dx++) {
+                const x = -this.tilesCacheOffsetX + dx;
+                const y = -this.tilesCacheOffsetY + dy;
+                const renderedTile = this.renderTile(x, y, false);
+                const cacheIndex = dy * this.tilesCacheWidth + dx;
+                this.tilesCache[cacheIndex] = renderedTile;
+            }
         }
     }
     render() {
@@ -153,39 +221,12 @@ export class AsciiRenderer {
             for (let dx = 0; dx < this.viewportWidth; dx++) {
                 const x = x0 + dx;
                 const y = y0 + dy;
-                const outOfBounds =
-                    x < 0 || x >= this.dungeon.width ||
-                    y < 0 || y >= this.dungeon.height;
-                const tileIndex = this.dungeon.getIndexFromCoords(x, y);
-                const isWall = outOfBounds ||
-                    this.dungeon.layoutTilesBuffer[tileIndex] === TileType.Wall;
-                const bgColor = isWall
-                    ? this.getWallBackgroundColor(x, y)
-                    : "default";
-                if (outOfBounds) {
-                    tiles.push(
-                        <RenderedTile
-                            key={dy * this.viewportWidth + dx}
-                            tilesPerRow={this.viewportWidth}
-                            color={tileForegroundColor(TileType.Wall)}
-                            backgroundColor={bgColor}
-                        >
-                            #
-                        </RenderedTile>
-                    );
+                if (this.isStatic(x, y)) {
+                    const cacheIndex = (this.tilesCacheOffsetY + y) * this.tilesCacheWidth + (this.tilesCacheOffsetX + x);
+                    const cachedValue = this.tilesCache[cacheIndex];
+                    tiles.push(cachedValue);
                 } else {
-                    const layoutTile = this.dungeon.layoutTilesBuffer[tileIndex];
-                    const entityLayers = this.dungeon.entityLayersBuffer[tileIndex];
-                    tiles.push(
-                        <RenderedTile
-                            key={dy * this.viewportWidth + dx}
-                            tilesPerRow={this.viewportWidth}
-                            color={tileForegroundColor(layoutTile, entityLayers)}
-                            backgroundColor={bgColor}
-                        >
-                            {tileSymbol(layoutTile, entityLayers)}
-                        </RenderedTile>
-                    );
+                    tiles.push(this.renderTile(x, y, true));
                 }
             }
         }
